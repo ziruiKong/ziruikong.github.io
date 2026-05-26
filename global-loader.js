97fba98c7aba697a45dc2e2f0aefc8e88b75616c
ziruiKong <ray20040626@gmail.com>
2026-05-26 10:15:26 +0800

    Switch global loader image source to loading.png

--- global-loader.js
+++ global-loader.js
@@ -8,7 +8,7 @@
     loader.className='page-loader';
     loader.id='globalPageLoader';
     loader.setAttribute('aria-hidden','true');
-    loader.innerHTML='\n      <div class="loader-shell">\n        <img class="loader-elephant" src="assets/kzr-shop-elephant-logo.png" alt="">\n        <div class="loader-text">LOADING</div>\n        <div class="loader-bar" aria-hidden="true"><span></span></div>\n      </div>';
+    loader.innerHTML='\n      <div class="loader-shell">\n        <img class="loader-elephant" src="assets/loading.png" alt="">\n        <div class="loader-text">LOADING</div>\n        <div class="loader-bar" aria-hidden="true"><span></span></div>\n      </div>';
     body.prepend(loader);
     window.setTimeout(function(){
       loader.classList.add('is-hidden');
